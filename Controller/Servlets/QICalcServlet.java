c35f6676b989d346f2e682ddf4881fec84104c0c
srikanthnarahari <srikanth.narahari@zoho.com>
2014-11-09 12:34:00 -0800

    Need to write to response

--- Controller/Servlets/QICalcServlet.java
+++ Controller/Servlets/QICalcServlet.java
@@ -1,6 +1,6 @@
-package Controller;
+package controller.servlets;
 
-import Model.RacquetBean;
+import java.io.IOException;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
@@ -11,35 +11,42 @@ This class does the under-the-hood work for the quality index calculator JSP.
 @version 2014/11/5
 */
 public class QICalcServlet extends HttpServlet {
-    /**
-    Calculates the quality index of a racquet.
-        Q = MR^2/I
-            where M = mass in kilograms,
-            r = cms from balance point to butt cap,
-            and I = swingweight.
-    @param racquet a Racquet object
-    @return the quality index of the racquet
+   /**
+    Handles the HTTP <code>POST</code> method.
+    @param request servlet request
+    @param response servlet response
+    @throws ServletException if a servlet-specific error occurs
+    @throws IOException if an I/O error occurs
     */
-    public double getQualityIndex (RacquetBean racquet)
-    {
-        // Q = MR^2/I
-            // where M = mass in kilograms,
-            // r = cms from balance point to butt cap,
-            // and I = swingweight.
-        final double M = (racquet.getMass() // ounces
-                / OZS_PER_LB) / LBS_PER_KG;
-        final double R = ((racquet.getLength() / 2) // inches
-                + (racquet.getBalancePoint() /* points */ / PTS_PER_IN))
-                / CMS_PER_IN;
-        final double I = racquet.getSwingweight();
-        
-        final double Q = M * R * R / I;
-        return Q;
-    }
+   @Override
+   protected void doPost(HttpServletRequest request,
+         HttpServletResponse response) throws ServletException, IOException {
+	// Get parameters.
+	final double MASS = Double.parseDouble(request.getParameter("mass"));
+	final double LENGTH = Double.parseDouble(request.getParameter("length"));
+	final double SWING_WEIGHT
+		= Double.parseDouble(request.getParameter("swingWeight"));
+	final double BALANCE_POINT
+		= Double.parseDouble(request.getParameter("balancePoint"));
+	
+	// Q = MR^2/I
+	   // where M = mass in kilograms,
+	   // r = cms from balance point to butt cap,
+	   // and I = swingweight.
+	final double M = (MASS // ounces
+		   / OZS_PER_LB) / LBS_PER_KG;
+	final double R = ((LENGTH / 2) // inches
+		+ (BALANCE_POINT /* points */ / PTS_PER_IN)) / CMS_PER_IN;
+	final double I = SWING_WEIGHT;
+      
+	final Double Q = M * R * R / I;
+
+	// TODO: write to response
+   }
+   
+   private static final double OZS_PER_LB = 16.0;
+   private static final double LBS_PER_KG = 2.2;
     
-    private static final double OZS_PER_LB = 16.0;
-    private static final double LBS_PER_KG = 2.2;
-    
-    private static final double CMS_PER_IN = 2.54;
-    private static final double PTS_PER_IN = 2.2;
+   private static final double CMS_PER_IN = 2.54;
+   private static final double PTS_PER_IN = 2.2;
 }
