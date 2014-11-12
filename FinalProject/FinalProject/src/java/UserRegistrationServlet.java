ed8b15790305a03882cf3ca7e95116785a1dd7db
jaypatel1392 <jaypatel1392@gmail.com>
2014-11-12 08:25:48 -0800

    UserRegistrationServlet updated
    New user cannot signup with already existing username. Each user name
    must be unique.

--- FinalProject/FinalProject/src/java/UserRegistrationServlet.java
+++ FinalProject/FinalProject/src/java/UserRegistrationServlet.java
@@ -3,6 +3,7 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,6 +20,7 @@ public class UserRegistrationServlet extends HttpServlet {
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -50,11 +52,28 @@ public class UserRegistrationServlet extends HttpServlet {
         String city = request.getParameter("city");
         String state = request.getParameter("state");
         final double RATING = 0.0;//Double.parseDouble(request.getParameter("rating"));
-        
+
         Connection conn;
         try {
             Class.forName("com.mysql.jdbc.Driver").newInstance();
             conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/the-dream-team", "root", "");
+
+            String usernameCheck = "Select count(userinfo.username) FROM userinfo WHERE userinfo.username = ?";
+            PreparedStatement st = conn.prepareStatement(usernameCheck);
+            st.setString(1, username);
+
+            ResultSet set = st.executeQuery();
+            boolean exist = set.next();
+
+            int userExist = 0;
+            if (exist) {
+                userExist = set.getInt(1);
+            }
+
+            if (userExist > 0) {
+                javax.swing.JOptionPane.showMessageDialog(null, "USER EXIST ALREADY");
+                response.sendRedirect("notregistered.jsp");
+            }
 
             String sql = "INSERT INTO userinfo(username, password, firstName, lastName, email, phoneNumber, city, state, rating)"
                     + "VALUES(?,?,?,?,?,?,?,?,?)";
@@ -71,20 +90,22 @@ public class UserRegistrationServlet extends HttpServlet {
             stmt.setString(8, state);
             stmt.setDouble(9, RATING);
 
-        /*
-        javax.swing.JOptionPane.showMessageDialog(null, "User Name: " + username);
-        javax.swing.JOptionPane.showMessageDialog(null, "Password: " + password);
-        javax.swing.JOptionPane.showMessageDialog(null, "first name: " + firstName);
-        javax.swing.JOptionPane.showMessageDialog(null, "last name: " + lastName);
-        javax.swing.JOptionPane.showMessageDialog(null, "email: " + email);
-        javax.swing.JOptionPane.showMessageDialog(null, "city: " + city);
-        javax.swing.JOptionPane.showMessageDialog(null, "state: " + state);
-        javax.swing.JOptionPane.showMessageDialog(null, "phone: " + phoneNumber);
-        */
+            /*
+             javax.swing.JOptionPane.showMessageDialog(null, "User Name: " + username);
+             javax.swing.JOptionPane.showMessageDialog(null, "Password: " + password);
+             javax.swing.JOptionPane.showMessageDialog(null, "first name: " + firstName);
+             javax.swing.JOptionPane.showMessageDialog(null, "last name: " + lastName);
+             javax.swing.JOptionPane.showMessageDialog(null, "email: " + email);
+             javax.swing.JOptionPane.showMessageDialog(null, "city: " + city);
+             javax.swing.JOptionPane.showMessageDialog(null, "state: " + state);
+             javax.swing.JOptionPane.showMessageDialog(null, "phone: " + phoneNumber);
+             */
             int i = stmt.executeUpdate();
-            
+
             if (i != 0) {
-                response.sendRedirect("registered.jsp");
+                javax.swing.JOptionPane.showMessageDialog(null, "Inside the Final If condition, before index is called");
+                //response.sendRedirect("registered.jsp");
+                response.sendRedirect("index.jsp");
             } else {
                 javax.swing.JOptionPane.showMessageDialog(null, "gets in else part: value of i is : " + i);
                 response.sendRedirect("notregistered.jsp");
