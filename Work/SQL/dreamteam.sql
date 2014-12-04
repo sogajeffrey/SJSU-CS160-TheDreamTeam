040d27c0fa8e153123a4814a5a3e297a71fd32f3
sogajeffrey <sogajeffrey@gmail.com>
2014-12-03 23:00:17 -0800

    more cleanup

--- Work/SQL/dreamteam.sql
+++ Work/SQL/dreamteam.sql
@@ -1,30 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 4.2.10.1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Nov 30, 2014 at 11:10 PM
--- Server version: 5.6.21-log
--- PHP Version: 5.3.28
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
---
--- Database: `dreamteam`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `listinginfo`
---
 
 CREATE TABLE IF NOT EXISTS `listinginfo` (
 `listingID` int(11) NOT NULL,
@@ -33,24 +14,20 @@ CREATE TABLE IF NOT EXISTS `listinginfo` (
   `price` double NOT NULL,
   `neworUsed` enum('New','Used') NOT NULL DEFAULT 'Used',
   `dateListed` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `dateSold` date DEFAULT NULL,
-  `usernameBuyer` varchar(50) DEFAULT NULL,
   `description` varchar(1024) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `listinginfo`
---
-
-INSERT INTO `listinginfo` (`listingID`, `username`, `racquetID`, `price`, `neworUsed`, `dateListed`, `dateSold`, `usernameBuyer`, `description`) VALUES
-(1, 'srkanth', 8, 94.99, 'New', '2014-11-30 23:33:16', NULL, NULL, 'Exclusive offer! Save big bucks on a silky smooth stick! For a limited time, free custom stringjob!'),
-(4, 'srkanth', 1, 79.99, 'Used', '2014-11-30 23:26:09', NULL, NULL, 'Solid offering in good condition. Minor cosmetic damage near throat.');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `racquetinfo`
---
+INSERT INTO `listinginfo` (`listingID`, `username`, `racquetID`, `price`, `neworUsed`, `dateListed`, `description`) VALUES
+(1, 'srkanth', 1, 79.99, 'Used', '2014-11-30 23:26:09', 'Solid offering in good condition. Minor cosmetic damage near throat.'),
+(2, 'ironman', 2, 799.99, 'Used', '2014-12-01 18:42:23', 'Exquisite offering! Rafael Nadal''s racquet of choice, signed by Rafa himself at the 2011 French Open!'),
+(3, 'jayp', 3, 159.99, 'Used', '2014-12-01 18:44:30', 'Novak Djokovic''s racquet of choice. I used for just one tournament.'),
+(4, 'jsu', 4, 89.99, 'Used', '2014-12-01 18:45:09', 'Andy Murray''s racquet. Bumper guard needs to be replaced. Other than that, great condition!'),
+(5, 'shub', 5, 59.99, 'New', '2014-12-01 18:46:47', 'Clearance sale!'),
+(6, 'etam', 6, 179.99, 'New', '2014-12-01 18:50:50', 'The perfect blend of stability and maneuverability!'),
+(7, 'etam', 7, 69.99, 'Used', '2014-12-01 18:52:58', 'Underrated racquet. Minimal cosmetic damage.'),
+(8, 'srkanth', 8, 94.99, 'New', '2014-12-04 23:33:16', 'Exclusive offer! Save big bucks on a silky smooth stick! For a limited time, free custom stringjob!'),
+(9, 'shub', 9, 249.99, 'New', '2014-12-01 18:57:09', 'Limited offer! Strung with the Lendl pattern for enhanced control. Scalpel-like stability and precision on groundstrokes and volleys alike.'),
+(10, 'jayp', 10, 39.99, 'Used', '2014-12-01 19:00:15', 'Great racquet, but the string broke and I don''t want to deal with it.');
 
 CREATE TABLE IF NOT EXISTS `racquetinfo` (
 `racquetID` int(11) NOT NULL,
@@ -63,27 +40,17 @@ CREATE TABLE IF NOT EXISTS `racquetinfo` (
   `qualityIndex` double DEFAULT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `racquetinfo`
---
-
 INSERT INTO `racquetinfo` (`racquetID`, `modelName`, `brand`, `mass`, `length`, `swingweight`, `balancePoint`, `qualityIndex`) VALUES
 (1, 'Pure Drive', 'Babolat', 11.1, 27, 308, -4, 1.116),
 (2, 'AeroPro Drive', 'Babolat', 11.3, 27, 316, -4, 1.108),
 (3, 'Graphene Speed MP', 'Head', 11, 27, 320, -3, 1.085),
-(4, 'Graphene Radical Pro', 'Head', 11.5, 127, 326, -6, 1.051),
+(4, 'Graphene Radical Pro', 'Head', 11.5, 27, 326, -6, 1.051),
 (5, 'Microgel Radical MP', 'Head', 11, 27, 315, -2, 1.124),
 (6, 'EX03 Tour 100 16x18', 'Prince', 11.6, 27, 323, -7, 1.049),
-(7, 'Organic 9', 'Volkl', 11.4, 27, 313, -5, 1.107),
+(7, 'Organix 9', 'Volkl', 11.4, 27, 313, -5, 1.107),
 (8, 'Blade 93', 'Wilson', 12, 27, 333, -6, 1.074),
 (9, 'VCore Tour 89', 'Yonex', 12.1, 27, 335, -8, 1.034),
 (10, 'Ezone Xi 98', 'Yonex', 11.5, 27, 324, -6, 1.058);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `userinfo`
---
 
 CREATE TABLE IF NOT EXISTS `userinfo` (
   `username` varchar(50) NOT NULL,
@@ -96,65 +63,34 @@ CREATE TABLE IF NOT EXISTS `userinfo` (
   `state` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `userinfo`
---
-
 INSERT INTO `userinfo` (`username`, `password`, `firstName`, `lastName`, `email`, `phoneNumber`, `city`, `state`) VALUES
 ('etam', '123456', 'Eric', 'Tam', 'etam@ghoo.com', '415-999-8876', 'San Jose', 'California'),
-('ironman', '123456', 'Tony', 'stark', 'info@stark.com', '415-999-8872', 'San Jose', 'California'),
-('jayp', '123456', 'Jay', 'Patel', 'jay@hostname.com', '415-666-8877', 'santa clara', 'California'),
-('jsu', '123456', 'Jeffrey', 'Su', 'su@life.com', '415-999-8177', 'San Jose', 'California'),
-('shub', '123456', 'Shubha', 'Ranganathan', 'shub@email.com', '415-999-8879', 'San Jose', 'California'),
-('srkanth', '123456', 'Srikanth', 'Narahari', 'sri@sjsu.com', '415-999-8877', 'san jose', 'California'),
-('test1', '123456', 'test1', 'man', 'test1@man.com', '123-456-3423', 'San Jose', 'California'),
-('test2', '123456', 'test2', 'man', 'test2@man.com', '123-456-3423', 'San Jose', 'California'),
-('test3', '123456', 'test3', 'man', 'test3@man.com', '123-456-3423', 'San Jose', 'California'),
-('test4', '123456', 'test4', 'man', 'test4@man.com', '123-456-3423', 'San Jose', 'California');
+('ironman', 'iamironman', 'Tony', 'Stark', 'info@stark.com', '415-999-8872', 'San Jose', 'California'),
+('jayp', 'mypassword', 'Jay', 'Patel', 'jay@hostname.com', '415-666-8877', 'Santa Clara', 'California'),
+('jsu', 'dreamteam', 'Jeffrey', 'Su', 'su@life.com', '415-999-8177', 'San Jose', 'California'),
+('lantan', 'liketocode', 'Lauren', 'Tanner', 'ltanner@yahoo.com', '123-456-3423', 'San Jose', 'California'),
+('marlee', 'asmr3322', 'Maria', 'Lee', 'marialee@hotmail.com', '123-456-3457', 'San Jose', 'California'),
+('shub', '65pass56', 'Shubha', 'Ranganathan', 'shub@email.com', '415-999-8879', 'San Jose', 'California'),
+('srkanth', 'srknar', 'Srikanth', 'Narahari', 'sri@sjsu.com', '415-999-8877', 'San Jose', 'California'),
+('tbran', 'brant', 'Brandon', 'Thomas', 'tbran@gmail.com', '123-456-3421', 'San Jose', 'California'),
+('tennislover', 'ilovetennis', 'Mark', 'Wheeler', 'mark.wheeler@gmail.com', '123-456-3429', 'San Jose', 'California');
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `listinginfo`
---
 ALTER TABLE `listinginfo`
  ADD PRIMARY KEY (`listingID`), ADD KEY `username` (`username`), ADD KEY `racquetID` (`racquetID`);
 
---
--- Indexes for table `racquetinfo`
---
 ALTER TABLE `racquetinfo`
  ADD PRIMARY KEY (`racquetID`);
 
---
--- Indexes for table `userinfo`
---
 ALTER TABLE `userinfo`
  ADD PRIMARY KEY (`username`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `listinginfo`
---
 ALTER TABLE `listinginfo`
-MODIFY `listingID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT for table `racquetinfo`
---
+MODIFY `listingID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
 ALTER TABLE `racquetinfo`
 MODIFY `racquetID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
---
--- Constraints for dumped tables
---
 
---
--- Constraints for table `listinginfo`
---
 ALTER TABLE `listinginfo`
 ADD CONSTRAINT `listinginfo_ibfk_1` FOREIGN KEY (`username`) REFERENCES `userinfo` (`username`) ON DELETE NO ACTION ON UPDATE NO ACTION,
 ADD CONSTRAINT `listinginfo_ibfk_2` FOREIGN KEY (`racquetID`) REFERENCES `racquetinfo` (`racquetID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
